94a1f7833d30da7e10d860e7b00c783a2c07d9b0
Christian Andrade <135077427+ChristiAndradee@users.noreply.github.com>
2025-06-07 16:12:22 -0300

    Adição de terminais e tempo

--- Main.c
+++ Main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
 
-int main(){
-    
-}
+#define V 10
+
+// Terminais de Curitiba
+const char *terminais[V] = {
+    "Boqueirão", "CIC", "Pinheirinho", "Cabral", "Guadalupe",
+    "Capão Raso", "Santa Cândida", "Fazendinha", "Campo Comprido", "Hauer"
+};
+
+// Matriz de tempo entre terminais
+int tempo[V][V] = {
+    {0, 10, 15, 0, 0, 20, 0, 0, 0, 25},
+    {10, 0, 5, 0, 0, 15, 0, 10, 0, 0},
+    {15, 5, 0, 0, 0, 5, 0, 0, 20, 0},
+    {0, 0, 0, 0, 10, 0, 5, 0, 0, 0},
+    {0, 0, 0, 10, 0, 0, 0, 0, 0, 5},
+    {20, 15, 5, 0, 0, 0, 0, 10, 0, 0},
+    {0, 0, 0, 5, 0, 0, 0, 15, 0, 0},
+    {0, 10, 0, 0, 0, 10, 15, 0, 10, 0},
+    {0, 0, 20, 0, 0, 0, 0, 10, 0, 5},
+    {25, 0, 0, 0, 5, 0, 0, 0, 5, 0}
+};
